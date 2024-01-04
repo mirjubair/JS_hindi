92c58ce3efd07c61afbd3a259d124f0620c8a9a5
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-04 15:46:33 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,3 +5,23 @@
 console.log(3+3)
  // code readibility must be high
 console.log(3+7)
+
+// Datatypes
+let name="jubair"
+let age=18
+let isLoggedIn=false
+
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value assign nhi huvbi hai
+// symbol => unique
+
+// object
+
+console.log(typeof"jubair")
+// console.log(typeof null)  // object
+// console.log(typeof undefined); // undefined
