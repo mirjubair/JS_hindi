f6796cd2bfbc023ab2ed5f69d372686513f9e15b
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-05 11:55:34 +0000

    Datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+// "33" => 33
+// "33abc"=>NaN
+// true => 1; false => 0
+
+let isLoggedIn = "jubi"
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// 1 => true; 0=> false
+// "" => false
+// "jubair" => true
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
