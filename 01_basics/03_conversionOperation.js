d11571e216b8256810911c9fdcd3a66576e7dc7f
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-06 06:27:21 +0000

    conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,25 +1,65 @@
-let score = true
+// let score = true
 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber= Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// let valueInNumber= Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 // "33" => 33
 // "33abc"=>NaN
 // true => 1; false => 0
 
-let isLoggedIn = "jubi"
+// let isLoggedIn = "jubi"
 
-let booleanisLoggedIn=Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
 // 1 => true; 0=> false
 // "" => false
 // "jubair" => true
 
-let someNumber=33
-let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber= String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************ Operations ************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " Jubair"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// not recommended
+// console.log(+true)
+// console.log(+"")
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2   // not recommended
+
+// let gameCounter = 100
+ 
+//  let newScore=gameCounter++;
+//  let pscore=++gameCounter;
+//  console.log(gameCounter);
+//  console.log(newScore);
+//  console.log(pscore);
+
+
