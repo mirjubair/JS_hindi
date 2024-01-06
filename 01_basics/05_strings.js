db4a4a42c181108f88885e6c8408b9e7cb85075a
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-06 15:08:55 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+// const name="jubair"
+// const repocount= 50
+// console.log(`Hello, my name is ${name.toUpperCase()} and my repocount is ${repocount} `);
+
+const gameName= new String("jubi-Don-greatest-technician-that-ever-lived")
+
+
+// console.log(gameName[0])
+// console.log(gameName[1])
+// console.log(gameName[2])
+// console.log(gameName[3])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("D"))
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherstring = gameName.slice(-6,6)
+console.log(anotherstring);
+
+const newStringOne="   Jubair   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://jubi.com//insta20"
+
+console.log(url.replace('20','-'));
+
+
+console.log(url.includes('jubi'))
+
+console.log(gameName.split('-'));
