808b4b41fc97a9c268d6dff89b5529f09f48aff8
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-06 06:36:59 +0000

    comparison datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 2);
+// console.log(2 < 2);
+// console.log(2 >= 2);
+// console.log(2 <= 2);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 1);
+console.log(null >= 0);
+// avoid  all these confusions
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// === strict check : checks datatype also
+console.log("2" === 2);
