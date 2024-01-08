87df62bb155fdf890cc36007fa44936432c7e021
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-08 05:27:08 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,15 @@
+const score = 400 
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const anotherNumber = 123.8966
+
+console.log(anotherNumber.toPrecision(3));
+
+const hundreds
