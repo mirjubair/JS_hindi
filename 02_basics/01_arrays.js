b041540bf49747eb1bfda81d95a1af54e5dfa84d
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-08 13:20:40 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+//    Array
+
+// const myArr = [0,1,2,34,5,6,"jubair"]
+const myArr = [0,1,2,34,5,6]
+
+console.log(myArr[0]);
+console.log(myArr[2]);
+
+const myHeros = ["superman","ironman","winter soldier"]
+const myArr2 = new Array(1,2,3,4,5)
+console.log(myArr2[0]);
