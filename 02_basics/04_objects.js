0bb0fe6fd41fa05fcb39e39308ea104f8f00e937
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-09 15:04:04 +0000

    objects part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,12 @@
+// const tinderUser = new Object() // singleton obj
+const tinderUser = {}     // non singleton obj
+
+tinderUser.id = "12234abc"
+tinderUser.name = "Jubair"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email :"some@gmail.com"
+}
