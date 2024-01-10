e9f3636213dc08bc673fca79f3d7e44014420f7b
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-10 16:41:44 +0000

    functions in depth

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,39 @@ function loginUserMessage(username = "jubi"){
 }
 
 // console.log(loginUserMessage("jubair")); 
-console.log(loginUserMessage()); 
+// console.log(loginUserMessage()); 
+                            // rest operator
+function calculateCartPrice(val1,val2,...num1){
+    console.log(`${val1} ${val2}`);
+    return num1
+
+}
+
+console.log(calculateCartPrice(200,400,500,2000));
+
+const user ={
+    username : "jubi",
+    price : 199
+
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username :"MJ",
+    price : 399
+})
+
+const myArray = [200,400,300,600]
+
+
+function returnSecondValue(array){
+
+    return array[1]
+
+}
+
+// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue([200,700,500,3000]));
