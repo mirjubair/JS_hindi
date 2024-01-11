2d75f656f8410424b639090bcf3f20fb0bd0b6d9
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-11 05:31:41 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,56 @@ let a = 300
 if (true) {             
     let a = 10          //  BLOCK
     const b = 20        //  SCOPE
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
                        //
      }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function  one(){
+    const username ="jubair"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);  // out of its scope
+
+    two()
+    
+}
+// one()
+if(true){
+    const username = "jubair"
+    if(username === "jubair"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++ interesting +++++++++++++++++
+
+
+// console.log(addOne(5)); still ok
+
+function addOne(num){
+    return num + 1
+
+}
+
+console.log(addOne(5));
+
+// console.log(addTwo(5));  // error as function is used as an expression (hoisting)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5));
+
