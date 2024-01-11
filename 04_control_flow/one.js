26b4e2c6bf2e0e0ea0b6c0213d66642b76cfb893
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-11 16:58:51 +0000

    control flow statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+//  if
+const isUserLoggedIn = true
+const Temperature = 41
+// if (Temperature < 50){
+
+//      console.log("executed");
+// }
+// else{
+// console.log("temperature is greater than 50");
+// }
+// // <,>,<=,>=,==,===(also checks type),!=   comparison operators
+
+// const score = 200
+
+// if ( score > 100){
+//     const power = "fly"
+//     console.log(`user power ${power}`);
+// }
+
+const balance = 1000
+// implicit scope
+// if ( balance > 500)  console.log("test");
+
+
+
+
