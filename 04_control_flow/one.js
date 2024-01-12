1a26a50678e92e6bf1923ca144384ca576fc3696
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-12 06:54:59 +0000

    control flow 1

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -17,10 +17,36 @@ const Temperature = 41
 //     console.log(`user power ${power}`);
 // }
 
-const balance = 1000
+// const balance = 1000
 // implicit scope
 // if ( balance > 500)  console.log("test");
 
+// if (balance < 500) {
+//     console.log("less than 500");
+
+// }
+// else if ( balance < 750){
+//     console.log("less than 500");
+     
+// }
+// else if(balance < 900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1100");
+// }
 
 
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
 
+if (userLoggedIn  && debitCard && 2==2){
+        console.log("allowed to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+
+    console.log("user logged in");
+}
