8cb397134d086877118b72ccbde5b49f9c6a7291
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-12 18:16:31 +0000

    truthy and falsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -18,13 +18,39 @@ else{
 
 
 */
-if (userEmail.length === 0) {
-    console.log("Array is empty");
-}
-const emptyObject= {}
-if (Object.keys(emptyObject).length === 0) {
-    console.log("Object is empty");
-}
+// if (userEmail.length === 0) {
+//     // console.log("Array is empty");
+// }
+// const emptyObject= {}
+// if (Object.keys(emptyObject).length === 0) {
+//     console.log("Object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15 
+val1 = null ?? 10 ?? 15
+// console.log(val1);
+
+// TERNARY OPERATOR
+
+// condition ? true statement : false statement
+
+const iceTeaPrice = 100
+
+iceTeaPrice >= 80 ? console.log("less than 80"): console.log("more than 80");;
+
+
+
+
+
+
+
+
 
 
 
