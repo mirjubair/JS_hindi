fe8f9957bc50de973116cfcdd6507acaaa0b2529
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-12 07:14:16 +0000

    switch statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch(month){
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("error");
+}
