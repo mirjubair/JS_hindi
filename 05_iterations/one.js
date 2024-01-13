1b1bd5484feb10d09f64be462c77585e149f95c7
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-13 06:47:34 +0000

    loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -11,11 +11,42 @@ for (let i = 0; i <= 10; i++) {
 
 // console.log(element);  out of scope
 
-for (let i = 0; i <= 5; i++) {
-    console.log(`outer loop value : ${i}`);
-    for (let j = 0; j <= 5; j++) {
-        console.log(`inner loop value : ${j} and inner loop ${i}`);
+for (let i = 1; i <= 10; i++) {
+    // console.log(`outer loop value : ${i}`);
+    for (let j = 1; j <= 10; j++) {
+       
+        // console.log(`inner loop value : ${j} and inner loop ${i}`);
+        // console.log(`${i} *  ${j} = ${i*j}`);
     }
 
 
 }
+
+let myArray =["flash","superman","batman"]
+//console.log(myArray.length)
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+  //  console.log(element);
+    
+}
+
+// break and continue
+
+// for (let index = 1; index <= 20 ; index++) {
+//     if(index == 5){
+//         console.log('5 detected');
+//         break
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+// for (let index = 1; index <= 20 ; index++) {
+//     if(index == 5){
+//         console.log('5 detected');
+//         continue
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// let i = 2
+// while (i<=40) {
+// console.log(`value of i is ${i}`);
+// i = i + 2
+    
+// }
+
+// let myArray = ["flash","superman" , "batman"]
+
+// let arr = 0
+// while(arr< myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+//let i = 11     // exit contolled
+let i = 1     // exit contolled
+sum = 0;
+do {
+    // console.log("loop still running")
+    sum = sum + i
+    i++ 
+} while (i<=10);
+console.log(sum)
