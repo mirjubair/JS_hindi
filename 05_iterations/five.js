924ba53c6b8e7a2c7aeba8e71c03f8d4f4e2da32
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-13 08:18:37 +0000

    higher order loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,15 @@
+const coding = ["js","cpp","java","python"] 
+
+// coding.forEach(function(item){
+//     console.log(item);
+// })
+
+// coding.forEach( (item) => {
+// console.log(item);
+// })
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe)
