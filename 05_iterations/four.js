e05eec74b5fbc9028e3f86f8e2d8493b78378b9b
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-01-13 08:09:22 +0000

    for of and for in

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,5 +6,22 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(`${key} ---> ${myObject[key]}`);
+    // console.log(`${key} ---> ${myObject[key]}`);
 }
+
+const programming = ['js','rb','cpp','java']
+
+for (const key in programming) {
+    // console.log(programming[key]);
+   
+}
+// const map = new Map()
+// map.set('IN',"india")
+// map.set('USA',"United States Of America")
+// map.set('Fr',"France")
+// map.set('Fr',"France")
+
+// for (const key in map) {   
+//     console.log(key);
+   
+// }   doesnt work as map is not iterable
