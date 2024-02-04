6f2564283551d6f3349c929249aa8191bddc0323
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-02-04 06:41:13 +0000

    nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -16,15 +16,15 @@
 // console.log(hundreds.toLocaleString('en-IN'));
 
 //  +++++++++++++++++++++ MATH +++++++++++++++++++
-// console.log(Math)
+console.log(Math)
 
-// console.log(Math.abs(-4));
-// console.log(Math.abs(4));
-// console.log(Math.round(4.5));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4,3,6,8));
-// console.log(Math.max(4,3,6,8));
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
 
 console.log(Math.random());  // range 0 - 1
 console.log((Math.random()*10) + 1);
