ae27e3f989a3b3760124892c26387ebe81942ca7
Mir Jubair Ul Rehman <126383560+mirjubair@users.noreply.github.com>
2024-02-04 08:49:13 +0000

    arrays part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -22,21 +22,21 @@ const myArr = [0,1,2,34,5,6]
 // console.log(myArr.includes(5));
 // console.log(myArr.indexOf(9));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join("-")
 // console.log(myArr);
 // console.log(newArr);
 // console.log(typeof newArr);
 
 // slice , splice
 
-// console.log("A ",myArr);
+console.log("A ",myArr);
 
-// const myn1= myArr.slice(1,3) // doesnt affect the original array and doeant include range
-// console.log(myn1);
-// console.log("B", myArr);
+const myn1= myArr.slice(1,3) // doesnt affect the original array and doeant include range
+console.log(myn1);
+console.log("B", myArr);
 
-// const myn2= myArr.splice(1,3) // includes range and does change the original array
-// console.log("C", myArr);
-// console.log(myn2);
+const myn2= myArr.splice(1,3) // includes range and does change the original array
+console.log("C", myArr);
+console.log(myn2);
 
 
